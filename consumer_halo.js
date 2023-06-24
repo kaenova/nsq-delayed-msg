ecc7f5d599120ed9d8834da39e543faca54553d6
Kaenova Mahendra Auditama <kaenova@gmail.com>
2023-06-24 22:01:23 +0700

    multiple channel

---
+++ consumer_halo.js
@@ -0,0 +1,24 @@
+import { Reader } from "nsqjs"
+
+async function main() {
+  const nsqClientReader = new Reader('delayed_topic', 'console_halo', {
+    lookupdHTTPAddresses: 'nsqlookupd:4161'
+  })
+
+  nsqClientReader.connect()
+
+  nsqClientReader.on("ready", () => {console.log("consumer ready")})
+  
+  nsqClientReader.on("error", () => {console.log("consumer error")})
+
+  nsqClientReader.on("message", (msg) => {
+    let date = new Date()
+    let finalMsg = msg.body.toString()
+    console.log(`[halo] ${finalMsg} recieved ${date.toLocaleTimeString()}`)
+    msg.finish()
+  })
+
+  console.log('done')
+}
+
+main()
--- consumer_hi.js
+++ consumer_hi.js
@@ -1,7 +1,7 @@
 import { Reader } from "nsqjs"
 
 async function main() {
-  const nsqClientReader = new Reader('delayed_topic', 'test_channel', {
+  const nsqClientReader = new Reader('delayed_topic', 'console_hi', {
     lookupdHTTPAddresses: 'nsqlookupd:4161'
   })
 
@@ -14,9 +14,11 @@ async function main() {
   nsqClientReader.on("message", (msg) => {
     let date = new Date()
     let finalMsg = msg.body.toString()
-    console.log(`${finalMsg} recieved ${date.toLocaleTimeString()}`)
+    console.log(`[hi] ${finalMsg} recieved ${date.toLocaleTimeString()}`)
     msg.finish()
   })
+
+  console.log('done')
 }
 
 main()
